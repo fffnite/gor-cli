f83999d02ff89db8b8652b88f37e35b58e1f0398
fffnite <bradley.whiteford@live.co.uk>
2019-06-10 01:45:24 +0100

    Add project files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Go OneRoster CLI tool
+
+Used for managing credentials for the [go-oneroster](https://github.com/fffnite/go-oneroster) api project.
+
+## Arguments
+
+`-new` Creates a new set of credentials
+`-tag` Used with `-new`, attaches human readable tag to the credential
+`-list` lists all credentials held
+
+### TODO
+
+`-remove` removes a credential from the store
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"crypto/rand"
+	"database/sql"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"github.com/Masterminds/squirrel"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	CreateTable = `CREATE TABLE IF NOT EXISTS creds (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		client_id TEXT,
+		client_secret_hash BLOB,
+		tag TEXT
+		)`
+	flagNew  = flag.Bool("new", false, "Create new creds")
+	flagTag  = flag.String("tag", "notSet", "human readable tag for new cred")
+	flagList = flag.Bool("list", false, "List existing credentials")
+)
+
+type Result struct {
+	Client_Id string
+	Tag       string
+}
+
+func main() {
+
+	db := ConnectDatabase()
+	defer db.Close()
+	db.Exec(CreateTable)
+	flag.Parse()
+	if *flagNew {
+		New(db, *flagTag)
+	}
+	if *flagList {
+		list(db)
+	}
+}
+
+func New(db *sql.DB, tag string) {
+	u, err := randomHex(25)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	p, err := randomHex(32)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("IMPORTANT: store somewhere safe as this is not recoverable")
+	fmt.Printf("client_id: %v \n", u)
+	fmt.Printf("client_secret: %v \n", p)
+	en := encrypt(p)
+	_, err = squirrel.
+		Insert("creds").
+		Columns("client_id", "client_secret_hash", "tag").
+		Values(u, en, tag).
+		RunWith(db).
+		Exec()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	return
+}
+
+func encrypt(s string) []byte {
+	b := []byte(s)
+	en, err := bcrypt.GenerateFromPassword(b, 10)
+	if err != nil {
+		fmt.Println(err)
+		n := make([]byte, 0)
+		return n
+	}
+	return en
+}
+
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func list(db *sql.DB) {
+	rows, err := squirrel.
+		Select("client_id", "tag").
+		From("creds").
+		OrderBy("id").
+		RunWith(db).
+		Query()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var results []Result
+	for rows.Next() {
+		var r Result
+		err := rows.Scan(&r.Client_Id, &r.Tag)
+		if err != nil {
+			fmt.Println(err)
+		}
+		results = append(results, r)
+	}
+	fmt.Printf("list: %v", results)
+}
+
+func ConnectDatabase() *sql.DB {
+	conn, err := sql.Open("sqlite3", "credstore")
+	if err != nil {
+		fmt.Println(err)
+	}
+	return conn
+}
